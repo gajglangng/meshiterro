9a8fa7659925b4ae76c3ce62d6b12d1282d2b869
EC2 Default User <ec2-user@ip-172-31-2-245.ap-northeast-1.compute.internal>
2022-08-13 07:19:00 +0000

    chapter9

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  
+ 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
-  get 'homes/about' => 'about'
+  devise_for :users
+  
+  #get 'post_images/new'
+  #get 'post_images/index'
+  #get 'post_images/show'の3つを書き換え
+  resources :post_images, only: [:new, :index, :show]
+  
+  get "/homes/about" => "homes#about", as: "about"
 end
--- db/migrate/20220813063730_create_post_images.rb
+++ db/migrate/20220813063730_create_post_images.rb
@@ -2,9 +2,8 @@ class CreatePostImages < ActiveRecord::Migration[6.1]
   def change
     create_table :post_images do |t|
       #以下の2行を追加
-      t.string :shop_name
-      t.text :caption
-
+     t.text :shop_name
+     t.text :caption
 
       t.timestamps
     end
